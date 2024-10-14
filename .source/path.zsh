d14333c2a2d3049a5d3a569b45e73f4118edda82
ag <akshith.573@gmail.com>
2024-10-14 16:49:11 -0700

    x
    x

--- .source/path.zsh
+++ .source/path.zsh
@@ -3,3 +3,15 @@
 path() {
     echo ${PATH} | tr ':' '\n'
 }
+
+appendtopath() {
+    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+        export PATH="$PATH:$1"
+    fi
+}
+
+prependtopath() {
+    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+        export PATH="$1:$PATH"
+    fi
+}
