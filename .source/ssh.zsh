cc9a6766a383c829e902b0f358d0816ce297cd02
ag <akshith.573@gmail.com>
2024-10-15 00:19:24 -0700

    x

--- .source/ssh.zsh
+++ .source/ssh.zsh
@@ -1,9 +1,11 @@
 # ssh-agent
 
-# start ssh-agent
-if [ -z ${SSH_AUTH_SOCK} ] ; then
-    eval "$(ssh-agent)" > /dev/null 2>&1
-fi
+# start ssh-agent if not already running
+# and only on local machine
+# use Agent Forwarding instead for other hosts
 
-# add keys to agent
-ls ~/.ssh/id_* | grep -v '.pub' | xargs ssh-add > /dev/null 2>&1
+if [ -z "$(pgrep ssh-agent)" ] && [ -z "$SSH_CLIENT" ]; then
+    eval "$(ssh-agent)" > /dev/null 2>&1
+    # add keys to agent
+    ls ~/.ssh/id_* | grep -v '.pub' | xargs ssh-add > /dev/null 2>&1
+fi
